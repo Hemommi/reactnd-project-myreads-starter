5cfc33eb3deb8ac6ee16f1626f4cbb2d99051884
Sandra Czerkas <sandra.czerkas@gmail.com>
2018-08-20 20:54:30 -0400

    'SearchBooks: input, import, showingBooks, '

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -4,26 +4,49 @@ import * as BooksAPI from './BooksAPI'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import BookShelfChanger from './BookShelfChanger'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class SearchBooks extends React.Component {
   state = {
-    books: []
+    books: [],
+    query:''
   }
   static propTypes = {
     books: PropTypes.array.isRequired
 }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
     render() {
+      let showingBooks
+        if(this.state.query) {
+          const match = new RegExp(escapeRegExp(this.state.query), 'i')
+          showingBooks = this.props.books.filter((books) => match.test(books.title))
+        }else{
+          showingBooks = this.props.books
+        }
+
+        showingBooks.sort(sortBy('title'))
+
         return (
             <div className="search-books">
             <div className="search-books-bar">
             <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
+                <input 
+                  type="text" 
+                  placeholder="Search by title or author"
+                  value={this.state.query}
+                  onChange={(event) => this.updateQuery(event.target.value)}
+                  />
               </div>
             </div>
             <div className="search-books-results">
               <ol className="books-grid">
-                {this.props.books.map((book) =>(
+                {showingBooks.map((book) =>(
                   <li className="book" key={book.id}>
                     <div className="book-top">
                       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")`}}></div>
